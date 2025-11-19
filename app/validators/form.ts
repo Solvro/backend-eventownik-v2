2e47df5b23686edfbdd9e5820a883c39bade17ca
K0smalka <284325@student.pwr.edu.pl>
2025-11-19 20:57:24 +0100

    refactor: add offset

--- app/validators/form.ts
+++ app/validators/form.ts
@@ -1,8 +1,8 @@
 import vine from "@vinejs/vine";
 import { DateTime } from "luxon";
 
-function dateTimeTransform(value: string): DateTime {
-  const parsed = DateTime.fromISO(value, { setZone: true });
+function dateTimeTransform(value: Date): DateTime {
+  const parsed = DateTime.fromJSDate(value);
   if (!parsed.isValid) {
     throw new Error("Invalid date");
   }
@@ -13,7 +13,9 @@ export const createFormValidator = vine.compile(
   vine.object({
     name: vine.string(),
     description: vine.string(),
-    startDate: vine.string().transform(dateTimeTransform),
+    startDate: vine
+      .date({ formats: { utc: true } })
+      .transform(dateTimeTransform),
     isFirstForm: vine.boolean(),
     attributes: vine
       .array(
@@ -25,7 +27,10 @@ export const createFormValidator = vine.compile(
         }),
       )
       .minLength(1),
-    endDate: vine.string().transform(dateTimeTransform).optional(),
+    endDate: vine
+      .date({ formats: { utc: true } })
+      .transform(dateTimeTransform)
+      .optional(),
     isOpen: vine.boolean().optional(),
   }),
 );
@@ -34,8 +39,14 @@ export const updateFormValidator = vine.compile(
   vine.object({
     name: vine.string().optional(),
     description: vine.string().optional(),
-    startDate: vine.string().transform(dateTimeTransform).optional(),
-    endDate: vine.string().transform(dateTimeTransform).optional(),
+    startDate: vine
+      .date({ formats: { utc: true } })
+      .transform(dateTimeTransform)
+      .optional(),
+    endDate: vine
+      .date({ formats: { utc: true } })
+      .transform(dateTimeTransform)
+      .optional(),
     attributes: vine
       .array(
         vine.object({
